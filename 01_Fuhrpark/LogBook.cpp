0b852f713d0363952c0eda62bd01bf840a146d74
marc.haunschmied <marc.haunschmied@hotmail.com>
2019-10-28 12:15:20 +0100

    Kommentare hinzugefügt

--- 01_Fuhrpark/LogBook.cpp
+++ 01_Fuhrpark/LogBook.cpp
@@ -2,7 +2,7 @@
 // Workfile :		LogBook.cpp
 // Author :			Marc Haunschmied
 // Date :			2019
-// Description :	
+// Description :	Kilometres driven on each day are saved in this LogBook
 // Revision :
 ///////////////////////////////////////////////////////////////////////////
 #include <iomanip>
@@ -14,12 +14,15 @@
 
 //Adds an Entry to the LogBook
 bool LogBook::AddEntry(std::string const& date, size_t const& distance) {
+
+	//check date format (YYMMDD) for example 191028
 	if (date.length() == 6 && std::all_of(date.begin(), date.end(), ::isdigit)) {
 
+		//find inserting Position to maintain sorting
 		auto insertPos = std::lower_bound(mEntries.cbegin(), mEntries.cend(), Entry{ date, 0 }, [](Entry const & e1, Entry const & e2) {
 				return e1.GetDate() < e2.GetDate();
 			});
-		
+
 		mEntries.insert(insertPos, Entry{ date,distance });
 		return true;
 	}
@@ -28,6 +31,8 @@ bool LogBook::AddEntry(std::string const& date, size_t const& distance) {
 	}
 }
 
+
+//manipulator to format a date with padding zeros 
 std::ostream& format_date(std::ostream& ost) {
 	if (ost.good()) {
 		ost << std::setw(2) << std::setfill('0');
@@ -35,6 +40,7 @@ std::ostream& format_date(std::ostream& ost) {
 	return ost;
 }
 
+//prints all logbook entries with formatted date and distance
 void LogBook::PrintLog(std::ostream& ost) const {
 	if (ost.good()) {
 		for (Entry entry : mEntries) {
@@ -43,6 +49,7 @@ void LogBook::PrintLog(std::ostream& ost) const {
 	}
 }
 
+//calculates the sum of all logbook entries
 size_t LogBook::SumDistance() const {
 	return std::accumulate(mEntries.cbegin(), mEntries.cend(), 0, [](int sum, Entry const & entry2) {
 		return sum + entry2.GetDist();
