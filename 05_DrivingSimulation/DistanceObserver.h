f9be3cf5a17df7faaf4ed7b293156160b6d41b72
marc.haunschmied <marc.haunschmied@hotmail.com>
2019-12-05 20:22:26 +0100

    observer hinzugefügt

--- 05_DrivingSimulation/DistanceObserver.h
+++ 05_DrivingSimulation/DistanceObserver.h
@@ -3,12 +3,18 @@
 
 #include "Object.h"
 #include "DisplayObserver.h"
+#include "WindowsDisplay.h"
+#include "Car.h"
 
 class DistanceObserver : public Object, DisplayObserver{
 public:
+	DistanceObserver(std::shared_ptr<Car> pCar);
 	void Update() override;
 private:
 	size_t mDistance;
+
+	WindowsDisplay::SPtr mDigDisp;
+	std::weak_ptr<Car> mCar;
 };
 
 #endif
--- 05_DrivingSimulation/SpeedObserver.h
+++ 05_DrivingSimulation/SpeedObserver.h
@@ -1,11 +1,20 @@
 #ifndef SPEED_OBSERVER_H
 #define SPEED_OBSERVER_H
 
+#include <memory>
+
 #include "Object.h"
 #include "DisplayObserver.h"
+#include "WindowsDisplay.h"
+#include "Car.h"
 
 class SpeedObserver : public Object, public DisplayObserver{
+public:
+	SpeedObserver(std::shared_ptr<Car> pCar);
 	void Update() override;
+private:
+	WindowsDisplay::SPtr mAnaDisp;
+	std::weak_ptr<Car> mCar;
 };
 
 #endif
